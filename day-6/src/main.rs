61e686634b9212d312c36b2e4f515526b2177b11
Hernan G. Gonzalez <camus.sonico@gmail.com>
2023-12-07 16:48:50 -0300

    Day 6 - Part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -8,26 +8,27 @@ fn main() {
     Distance:   334   1135   1350   2430
     "#;
     let res1 = part_1::process(input).unwrap();
+    let res2 = part_2::process(input).unwrap();
     println!("Part 1: {res1}");
+    println!("Part 2: {res2}");
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
 struct Race {
-    time: u32,
-    dist: u32,
+    time: u64,
+    dist: u64,
 }
+#[derive(Debug)]
+struct Races(Vec<Race>);
 
-struct Boat(u32);
+struct Boat(u64);
 
 impl Boat {
-    fn distance_by(&self, t: u32) -> u32 {
+    fn distance_by(&self, t: u64) -> u64 {
         self.0 * t
     }
 }
-
-#[derive(Debug)]
-struct Races(Vec<Race>);
 
 impl IntoIterator for Races {
     type Item = Race;
@@ -46,11 +47,11 @@ impl FromStr for Races {
         let times = lines.next().context("missing times")?;
         let dists = lines.next().context("missing distances")?;
 
-        let parse_line = |l: &str| -> Result<Vec<u32>> {
+        let parse_line = |l: &str| -> Result<Vec<u64>> {
             let nums = l.splitn(2, ':').last().context("Empty line")?;
             let vec: Vec<_> = nums
                 .split_whitespace()
-                .flat_map(|w| w.parse::<u32>())
+                .flat_map(|w| w.parse::<u64>())
                 .collect();
             Ok(vec)
         };
@@ -69,6 +70,15 @@ impl FromStr for Races {
     }
 }
 
+impl Race {
+    fn possible_victories(&self) -> u32 {
+        (0..=self.time)
+            .map(|s| Boat(s).distance_by(self.time - s))
+            .filter(|d| *d > self.dist)
+            .count() as u32
+    }
+}
+
 mod part_1 {
     use super::*;
 
@@ -76,16 +86,23 @@ mod part_1 {
         let races = Races::from_str(text)?;
         races
             .into_iter()
-            .map(|race| {
-                (0..=race.time)
-                    .map(|s| Boat(s).distance_by(race.time - s))
-                    .filter(|d| *d > race.dist)
-                    .count()
-            })
-            .map(|c| c as u32)
+            .map(|race| race.possible_victories())
             .filter(|c| *c > 0)
             .reduce(|l, r| l * r)
             .context("No victories?")
+    }
+}
+
+mod part_2 {
+    use super::*;
+
+    pub fn process(text: &str) -> Result<u32> {
+        let text = text.replace(" ", "");
+        let races = Races::from_str(&text)?;
+        Ok(races
+            .into_iter()
+            .map(|race| race.possible_victories())
+            .sum())
     }
 }
 
@@ -102,5 +119,11 @@ mod tests {
     fn test_part_1() {
         let res = part_1::process(SAMPLE).unwrap();
         assert_eq!(res, 288);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let res = part_2::process(SAMPLE).unwrap();
+        assert_eq!(res, 71503);
     }
 }
